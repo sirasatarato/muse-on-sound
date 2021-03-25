cda3efecba2bd952852eeac223aa9c8331d94823
sirasatarato <sirasatarato@gmail.com>
2021-03-25 20:26:19 +0900

    Add Package and font

--- muse_on_sound/lib/main.dart
+++ muse_on_sound/lib/main.dart
@@ -5,9 +5,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Muse on Sound',
-      theme: ThemeData(primarySwatch: Colors.blue),
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(home: Splash());
+        } else {
+          return MaterialApp(
+            title: 'Muse on Sound',
+            theme: ThemeData(primarySwatch: Colors.blue),
+            home: Scaffold(
+              body: Container(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.apartment_outlined,
+          size: MediaQuery.of(context).size.width * 0.785,
+        ),
+      ),
     );
   }
 }
