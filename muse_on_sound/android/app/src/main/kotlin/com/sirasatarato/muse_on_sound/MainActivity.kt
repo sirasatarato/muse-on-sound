74229474915de4d106f11b3fab04fce6482d57f0
sirasatarato <sirasatarato@gmail.com>
2021-03-25 19:34:28 +0900

    Create Project

---
+++ muse_on_sound/android/app/src/main/kotlin/com/sirasatarato/muse_on_sound/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sirasatarato.muse_on_sound
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ muse_on_sound/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Muse on Sound',
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
