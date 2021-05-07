8c35717a3d76201a555f64a9bb82e203844a31f6
sirasatarato <sirasatarato@gmail.com>
2021-05-07 10:34:58 +0900

    Exchange FutureBuilder with GetMaterialApp

--- muse_on_sound/lib/main.dart
+++ muse_on_sound/lib/main.dart
@@ -3,31 +3,25 @@ import 'package:get/get.dart';
 
 import 'src/pages/page_frame.dart';
 import 'src/pages/splash_page.dart';
-import 'src/util/theme_data/bottom_navigation_bar_theme_data.dart';
-import 'src/util/theme_data/text_theme_data.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 2)),
-      builder: (context, AsyncSnapshot snapshot) => buildMaterialApp(snapshot),
-    );
-  }
-
-  GetMaterialApp buildMaterialApp(AsyncSnapshot snapshot) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Muse on Sound',
       theme: ThemeData(
-        bottomNavigationBarTheme: bottomNavigationBarThemeData,
-        textTheme: textThemeData,
+        accentColor: Color(0xFFDD0870)
       ),
-      debugShowCheckedModeBanner: false,
-      home: snapshot.connectionState == ConnectionState.waiting
-          ? SplashPage()
-          : PageFrame(),
+      home: FutureBuilder(
+        future: Future.delayed(Duration(seconds: 0)),
+        builder: (context, AsyncSnapshot snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? SplashPage()
+                : PageFrame(),
+      ),
     );
   }
 }
