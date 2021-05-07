bdb901e7254e7eff40d3753b374ec8f810e1455d
sirasatarato <sirasatarato@gmail.com>
2021-05-07 15:17:28 +0900

    Add Get All Audio Files

--- muse_on_sound/lib/src/controller/music_controller.dart
+++ muse_on_sound/lib/src/controller/music_controller.dart
@@ -1,8 +1,27 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_audio_query/flutter_audio_query.dart';
 import 'package:get/get.dart';
 import 'package:muse_on_sound/src/model/music_model.dart';
 
 class MusicController extends GetxController {
-  final _music = Music('Life is PIANO', 'Junk', '').obs;
+  final _selectedMusic = Music('Unknown', '<Unknown>').obs;
+  final _musicList = <SongInfo>[].obs;
 
-  Music get music => _music.value;
+  MusicController() {
+    FlutterAudioQuery().getSongs().then((value) {
+      print('find music: ${value.length}');
+      value.map((e) => e.title).forEach(print);
+      _musicList.value = value;
+    });
+  }
+
+  // ignore: invalid_use_of_protected_member
+  List<SongInfo> get musicList => _musicList.value;
+
+  Music get selectedMusic => _selectedMusic.value;
+
+  void setSelectedMusic(SongInfo songInfo) {
+    _selectedMusic.value =
+        Music(songInfo.title, songInfo.artist, cover: songInfo.id);
+  }
 }
